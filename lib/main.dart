9c4ea9e45be2683dea1939748195e19d2519f844
WILDGARO <you@example.comnawamin0322@gmail.com>
2020-09-09 15:31:20 +0700

    FBClone

--- lib/main.dart
+++ lib/main.dart
@@ -9,65 +9,143 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyAppState extends State<MyApp> { 
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-home: Scaffold(
-  body: Container(
-    height: double.infinity,
-    width: double.infinity,
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children:[
-        Container(
-          padding: EdgeInsets.only(bottom:50),
-          child:Image.asset("assets/Logo.png"),
-        width: 200,
-        height: 200,
-        ),
-        Container(
-          width: 300,
-          child: TextFormField(
-              decoration: InputDecoration(
-              hintText: "Username"
-              ),
-          ),
-          ),
-        Container(
-          width: 300,
-          child: TextFormField(
-            decoration: InputDecoration(
-              hintText: "Password"
+      home: Scaffold(
+        body: Page1(),
+      ),
+    );
+  }
+}
 
+class Page1 extends StatefulWidget {
+  @override
+  _Page1State createState() => _Page1State();
+}
+
+class _Page1State extends State<Page1> {
+
+  TextEditingController _username;
+  TextEditingController _password;
+
+  
+  //user and password use login this applica....
+  String usertrue = "ware";
+  String passtrue = "12345";
+
+  String user = "";
+  String pass = "";
+
+  @override
+    void initState() {
+      // TODO: implement initState
+      super.initState();
+      _username = TextEditingController();
+      _password = TextEditingController();
+    }
+  onlogin(){
+    print("object");
+    setState(() {
+      user = _username.text;
+      pass = _password.text;
+    });
+    print(user == usertrue);
+    onnavigator();
+    
+  }
+
+  onnavigator(){
+    if (user == usertrue && pass ==passtrue){
+      print("ddfdf");
+      Navigator.push(
+       context, 
+       MaterialPageRoute(builder: (context)=> AfterLogin()
+       )
+      ); 
+    }
+    else{
+      _password.clear();
+      _username.clear();
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+            height: double.infinity,
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(bottom: 50.0),
+                  child: Image.asset("assets/Logo.png"),
+                  width: 200,
+                  height: 200,
+                ),
+                Container(
+                  width: 400,
+                  child: TextFormField(
+                    controller: _username,
+                    decoration: InputDecoration(
+                      hintText: "Username"
+                    ),
+                  )
+                  ),
+                Container(
+                  width: 400,
+                  child: TextFormField(
+                    controller: _password,
+                    decoration: InputDecoration(
+                      hintText: "Password",
+                
+                    ),
+                    obscureText: true,
+                  )
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 20.0),
+                  child: FlatButton(
+                    onPressed: (){
+                      onlogin();                      
+                    },
+                    child: Container(
+                      color: Colors.blue,
+                      alignment: Alignment.center,
+                      width: 200,
+                      height: 50,
+                      child: Text(
+                        "Login",
+                        style: TextStyle(
+                          fontSize: 30.0,
+                          color: Colors.white
+                        ),
+                      ),
+                    ),
+                    
+                  ),
+                )
+              ],
             ),
-            obscureText: true,
-          )
-          ),
-        Container(
-          padding: EdgeInsets.only(top: 20),
-          child: RaisedButton(
-            color: Colors.blue,
-            onPressed: (){
+          );
+  }
+}
 
-            },
-            child: Container(
-              alignment: Alignment.center,
-              width: 150,
-              height: 50,
-              child: Text("Login",
-              style:TextStyle(
-                fontSize: 30,
-                color: Colors.white
-              )),
-            ),
-
-          ),
-        )
-      ],
-    ),
-  ),
-),
+class AfterLogin extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        alignment: Alignment.center,
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.white,
+        child: Text("login แล้วน่ะ"),
+      )
+    
     );
   }
 }
