86859537ba4052102bedb76e06682c9ff0b3ec1c
WILDGARO <you@example.comnawamin0322@gmail.com>
2020-09-10 21:00:48 +0700

    50%

--- lib/main.dart
+++ lib/main.dart
@@ -164,23 +164,6 @@ class AfterLogin extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Container(
-        alignment: Alignment.center,
-        width: double.infinity,
-        height: double.infinity,
-        color: Colors.white,
-        child: Text("login แล้วน่ะ"),
-      )
-    
-    );
-  }
-}
-
-class Page3 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
         body: Container(
           alignment: Alignment.center,
           child: Screen1(),
@@ -201,6 +184,150 @@ class Screen1 extends StatefulWidget {
 }
 
 class _Screen1State extends State<Screen1> {
+  String _message = "";
+  connectionMqtt()async{
+
+	//fixต้องใส่   เช็คการ connection
+    client.port = 1883;
+    client.onConnected = onConnected;
+    try {
+      await client.connect();
+    } on Exception catch (e) {
+      print('EXAMPLE::client exception - $e');
+      client.disconnect();
+    }
+    if (client.connectionStatus.state == MqttConnectionState.connected) {
+      print('EXAMPLE::Mosquitto client connected');
+    } else {
+      print(
+          'EXAMPLE::ERROR Mosquitto client connection failed - disconnecting, state is ${client.connectionStatus.state}');
+      client.disconnect();
+    }
+    final connMess = MqttConnectMessage()
+      .withClientIdentifier('Mqtt_MyClientUniqueId')
+      .startClean() // Non persistent session for testing
+      .withWillQos(MqttQos.atLeastOnce);
+    print('EXAMPLE::Mosquitto client connecting....');
+    client.connectionMessage = connMess;
+
+
+    //subscribe  ส่วนของการ connection หากต้องการ subscribe ให้ setstateตรงนี้  อย่าลืมนะ ใช้ stateful 
+    const topic = 'at2';
+    client.subscribe(topic, MqttQos.atMostOnce);
+    client.updates.listen((List<MqttReceivedMessage<MqttMessage>> c) {
+
+      final MqttPublishMessage recMess = c[0].payload;
+	//pt เป็นข้อความที่ได้รับมา หากจะรับและมาแสดงผล ให้ setstate ที่นี่
+      final pt =
+          MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+          setState(() {
+            _message = pt;
+          });
+
+      print(
+          'EXAMPLE::Change notification:: topic is <${c[0].topic}>, payload is <-- $pt -->');
+      print('');
+    });
+  }
+   void onConnected(){
+    print("......................connected...............");  
+  }
+
+  final ware2 = 'at2';
+  send456(){
+    final builder = MqttClientPayloadBuilder();
+    builder.addString('Sum');
+    client.publishMessage(ware2, MqttQos.exactlyOnce, builder.payload);
+    
+  }
+
+    final ware = 'at2';
+  send123(){
+    final builder = MqttClientPayloadBuilder();
+    builder.addString('Reset');
+    client.publishMessage(ware, MqttQos.exactlyOnce, builder.payload);
+
+  } 
+
+  @override
+
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "จำนวนเงินในขณะนี้ \n$_message",
+          style: TextStyle(
+            fontSize: 40.0
+          ),
+        ),
+        RaisedButton(
+          onPressed: (){
+             connectionMqtt();
+          },
+          child: Text(
+            "รีเฟรช",
+            style: TextStyle(
+              fontSize: 20,
+              color:Colors.black,
+            )
+          ),
+        ),
+
+          FlatButton(
+          onPressed: (){
+            send123();
+          }, 
+          child: Container(
+            alignment: Alignment.center,
+            height: 100.0,
+            width: 200,
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.all(Radius.circular(40),
+              )
+            ),
+            child: Text(
+              "รีเซ็ตจำนวนเงิน",
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.yellow,
+              ),
+            ),
+          )
+          ),
+      ],
+    );
+  }
+}
+
+
+
+class Page3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+        body: Container(
+          alignment: Alignment.center,
+          child: Screen2(),
+
+        ),
+      );
+    
+  }
+}
+
+
+class Screen2 extends StatefulWidget {
+  
+  
+  
+  @override
+  _Screen2State createState() => _Screen2State();
+}
+
+class _Screen2State extends State<Screen2> {
   String _message = "";
   connectionMqtt()async{
 
@@ -313,29 +440,6 @@ class _Screen1State extends State<Screen1> {
           )
           ),
 
-
-          FlatButton(
-          onPressed: (){
-            send123();
-          }, 
-          child: Container(
-            alignment: Alignment.center,
-            height: 100.0,
-            width: 200,
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.all(Radius.circular(40),
-              )
-            ),
-            child: Text(
-              "รีเซ็ตจำนวนเงิน",
-              style: TextStyle(
-                fontSize: 30,
-                color: Colors.yellow,
-              ),
-            ),
-          )
-          ),
       ],
     );
   }
